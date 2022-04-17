f498553c8e42eea3f614efe46c08e8a3873d8070
simidawalang <simidawalang@gmail.com>
2022-04-17 10:16:23 +0100

    configured hardhat.config

---
+++ contracts/QuCoin.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+pragma solidity ^0.8.9;
+
+contract QiCoin is ERC20, ERC20Burnable, Ownable{
+    event TokensBurned(address indexed owner, uint amount, string message);
+    event TokensMinted(address indexed owner, uint amount, string message);
+    event AdditionalTokensMinted(address indexed owner, uint amount, string message);
+
+    constructor() ERC20("QuCoin", "QUC") {
+        _mint(msg.sender, 1000 * 10**decimals());
+    }
+
+    function mint(address to, uint amount) public onlyOwner {
+        _mint(to, amount);
+        emit AdditionalTokensMinted(to, amount, "Minted additional tokens.");
+    }
+
+    function burn (uint amount) public override onlyOwner {     
+        _burn(msg.sender,amount);
+        emit TokensBurned(msg.sender, amount, "Burned tokens");
+    }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,5 @@
+import { ethers} from "hardhat";
+
+const deploy = async() => {
+  const [owner] = ethers.getSigners();
+}
